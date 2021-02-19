fafd08ab3adfc103a535b7bc7b12104c84f1abc1
Iain Brookes <iain.brookes@outlook.com>
2021-02-19 23:40:11 +0000

    added css and js file

---
+++ script.js
@@ -0,0 +1,6 @@
+const email = document.querySelector('#email');
+const country = document.querySelector('#country');
+const postalCode = document.querySelector('#postal-code');
+const createPassword = document.querySelector('#create-password');
+const confirmPassword = document.querySelector('#confirm-password');
+
