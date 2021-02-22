c02379b17c9d4fdbeca47e27921c540c35abe085
Iain Brookes <me@iainbrux.com>
2021-02-22 12:35:39 +0000

    JS validation API completed
    JS will now correctly validate input forms based off the HTML
    constraints.

--- script.js
+++ script.js
@@ -6,20 +6,24 @@ const confirmPassword = document.querySelector('#confirm-password');
 const submit = document.querySelector('#submit');
 
 email.addEventListener('input', () => {
-    email.validity.typeMismatch ? email.setCustomValidity('Invalid email format') : console.log('Success');
+    email.validity.typeMismatch ? email.setCustomValidity('Invalid email format') : email.setCustomValidity('');
 })
 
 postalCode.addEventListener('input', () => {
-    postalCode.validity.patternMismatch ? postalCode.setCustomValidity('Please enter a postcode in the format JE12 3YZ') : console.log('Success');
+    postalCode.validity.patternMismatch ? postalCode.setCustomValidity('Please enter a postcode in the format AB1 2YZ') : 
+	postalCode.setCustomValidity('');
 })
 
 createPassword.addEventListener('input', () => {
-    (createPassword.validity.rangeUnderflow || createPassword.validity.rangeOverflow) ? createPassword.setCustomValidity('Please enter a password between 8 and 30 characters') : console.log('Success');
-})
+	createPassword.validity.patternMismatch ? createPassword.setCustomValidity('Password must contain a capital letter, a number and a special character') : 
+	createPassword.setCustomValidity('');
+});
 
-submit.addEventListener('click', (e) => {
-    confirmPassword.value != createPassword.value ? ( e.preventDefault(),
-        confirmPassword.setCustomValidity('Passwords do not match')) 
-        : console.log('Success');
-    createPassword.validity.patternMismatch ? createPassword.setCustomValidity('Password must contain a number, a capital letter, and a symbol from either !@$&*#?') : console.log('Success');
-})
+confirmPassword.addEventListener('input', () => {
+    (createPassword.value != confirmPassword.value) ? confirmPassword.setCustomValidity('Passwords do not match') :
+	(confirmPassword.setCustomValidity(''), console.log('Password success.'))
+});
+
+submit.addEventListener('click', () => {
+    
+});
